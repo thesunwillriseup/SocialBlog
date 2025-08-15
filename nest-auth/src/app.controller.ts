06329f6f1380080d8114c88d4ff5944fc81787fa
CodeVoyager15 <31968103+CodeVoyager15@users.noreply.github.com>
2025-08-15 14:11:15 -0500

    Update app.controller.ts

--- nest-auth/src/app.controller.ts
+++ nest-auth/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';  // Import ApiTags for controller-level docs
 import { AppService } from './app.service';
 
+@ApiTags('App')  // Tags for the controller
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
